a378cc33a139386e3626dae893c63535c1932e8a
LLJ3288 <74459081+LLJ3288@users.noreply.github.com>
2021-03-11 06:58:39 -0600

    Files added.

---
+++ src/main/java/com/tts/mockweb/GreetingService.java
@@ -0,0 +1,10 @@
+package com.tts.mockweb;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+    public String greet() {
+        return "Hello, World";
+    }
+}
